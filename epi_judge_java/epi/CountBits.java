8e72218d39415842835d345b6a9d990987e4aa6d
nyurdakul <ny2266@columbia.edu>
2019-05-23 00:29:24 +0300

    coded CountBits

--- epi_judge_java/epi/CountBits.java
+++ epi_judge_java/epi/CountBits.java
@@ -1,12 +1,20 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class CountBits {
   @EpiTest(testDataFile = "count_bits.tsv")
 
   public static short countBits(int x) {
     // TODO - you fill in here.
-    return 0;
+
+    short bits = 0;
+    while(x != 0){
+      bits += (x & 1);
+      x >>>= 1;
+    }
+
+    return bits;
   }
 
   public static void main(String[] args) {
