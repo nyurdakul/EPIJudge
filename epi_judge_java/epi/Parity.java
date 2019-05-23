9750a5fa391a15da0e2573daa12297e107fa554b
nyurdakul <ny2266@columbia.edu>
2019-05-23 12:27:48 +0300

    coded Parity

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -3,10 +3,46 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class Parity {
   @EpiTest(testDataFile = "parity.tsv")
-  public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+
+  /*
+    Table-based approach:
+    - create a lookup table for parities of 16-bit words;
+    - divide the word into 4 parts by using bit shifts and masks
+    - add parities
+   */
+
+  /*
+  //O(n/L) where L is the hashed word size
+  public static short parity(long x){
+    final int W0RD_SIZE = 16; // 1111 1111 1111 1111
+    final int BIT_MASK = OxFFFF; return (short) (
+            precomputedParity[(int)((x »> (3 * W0RD_SIZE)) & BIT_MASK)]
+    A precomputedParity[(int)((x»>(2*W0RD_SIZE))&BIT_MASK)] A precomputedParity[(int)((x »> W0RD_SIZE) & BIT_MASK)]
+    A precomputedParity[(int)(x&BIT_MASK)]);
   }
+   */
+
+  //O(N)
+  public static short parity(long x) {
+    short result = 0;
+    while (x != 0) {
+      result ^= (x & 1);
+      x >>>= 1;
+    }
+    return result;
+  }
+
+  /*
+  //O(k) where k is the number of 1 bits
+  public static short parity(long x) {
+    short result = 9;
+    while (x != 9) {
+      result A = 1;
+      x &= (x - 1); // Drops the lowest set bit of x. >
+      return result;
+    }
+  }
+  */
 
   public static void main(String[] args) {
     System.exit(
