e8a98bfc4d7d992a67a38ba3dda4c62ac81acbd5
nyurdakul <ny2266@columbia.edu>
2019-06-18 00:32:23 -0400

    coded PowerXY (5.7)

--- epi_judge_java/epi/PowerXY.java
+++ epi_judge_java/epi/PowerXY.java
@@ -4,8 +4,20 @@ import epi.test_framework.GenericTest;
 public class PowerXY {
   @EpiTest(testDataFile = "power_x_y.tsv")
   public static double power(double x, int y) {
-    // TODO - you fill in here.
-    return 0.0;
+    double result = 1.0;
+    long power = y;
+    if(y < 0) {
+      power = -power;
+      x = 1.0 / x;
+    }
+    while (power != 0) {
+      if ((power & 1) != 0) {
+        result *= x;
+      }
+      x *= x;
+      power >>>= 1;
+    }
+    return result;
   }
 
   public static void main(String[] args) {
