8b59bacf2eb0be1d7a3cbbf041c8e9ad4b97f7ff
nyurdakul <ny2266@columbia.edu>
2019-06-19 00:50:06 -0400

    coded IsNumberPalindromic (5.9)

--- epi_judge_java/epi/IsNumberPalindromic.java
+++ epi_judge_java/epi/IsNumberPalindromic.java
@@ -4,7 +4,20 @@ import epi.test_framework.GenericTest;
 public class IsNumberPalindromic {
   @EpiTest(testDataFile = "is_number_palindromic.tsv")
   public static boolean isPalindromeNumber(int x) {
-    // TODO - you fill in here.
+    if(x < 0) return false;
+
+    int digitNum = (int) (Math.floor(Math.log10(x)));
+
+    while(digitNum > 0){
+      int leastSig = x % 10;
+      int mostSig = x / (int)(Math.pow(10, digitNum));
+      if (leastSig != mostSig) return false;
+
+      x %= (int)(Math.pow(10, digitNum));
+      x /= 10;
+      digitNum -= 2;
+    }
+
     return true;
   }
 
