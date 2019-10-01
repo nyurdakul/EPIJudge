b91f5b8e0ae808e6a3d45fbe5a8600ef905e9737
nyurdakul <ny2266@columbia.edu>
2019-10-01 13:21:46 -0400

    coded BuyAndSellStock (6.6)

--- epi_judge_java/epi/BuyAndSellStock.java
+++ epi_judge_java/epi/BuyAndSellStock.java
@@ -5,8 +5,17 @@ import java.util.List;
 public class BuyAndSellStock {
   @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
   public static double computeMaxProfit(List<Double> prices) {
-    // TODO - you fill in here.
-    return 0.0;
+    Double min = Double.MAX_VALUE;
+    Double profit = 0.0;
+    for(Double price : prices){
+      if(price < min){
+        min = price;
+      }
+      else if(price - min > profit){
+        profit = price - min;
+      }
+    }
+    return profit;
   }
 
   public static void main(String[] args) {
