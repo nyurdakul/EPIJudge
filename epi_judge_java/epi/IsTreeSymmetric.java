23bd8adb87f59f7b27274d2ea0838352141a6cf6
nyurdakul <ny2266@columbia.edu>
2019-11-03 21:57:36 -0500

    coded IsTreeSymmetric (10.2)

--- epi_judge_java/epi/IsTreeSymmetric.java
+++ epi_judge_java/epi/IsTreeSymmetric.java
@@ -5,8 +5,15 @@ public class IsTreeSymmetric {
   @EpiTest(testDataFile = "is_tree_symmetric.tsv")
 
   public static boolean isSymmetric(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    if(tree == null) return true;
+    return traverse(tree, tree);
+  }
+
+  private static boolean traverse(BinaryTreeNode<Integer> tree1, BinaryTreeNode<Integer> tree2){
+    if(tree1 == null && tree2 == null) return true;
+    if(tree1 == null || tree2 == null) return false;
+    if(tree1.data != tree2.data) return false;
+    return traverse(tree1.left, tree2.right) && traverse(tree1.right, tree2.left);
   }
 
   public static void main(String[] args) {
